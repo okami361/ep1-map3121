c9d5e1c0fcce39bc0ae82fb265df9d520cd13d3b
Vinicius Yu Okubo <vinnco@hotmail.com>
2020-04-21 13:39:38 -0300

    implementação ex1
    funcional(?)

---
+++ src/ex1.py
@@ -0,0 +1,26 @@
+class Ex1:
+    def resolver(self, u0, f, N_in, M_in, lambda_in, g1_in, g2_in):
+        
+        
+        delta_t = lambda_in/pow(N_in,2)
+
+        ut = []
+        t=0
+        ut.append(g1_in(t))
+
+        for x in range(1,N_in):
+            ut.append(u0(x))
+
+        ut.append(g2_in(t))
+
+        for y in range(0, M_in):
+            ut_next = ut
+            for x in range(1,N_in):
+                ut_next[x] = delta_t*((ut[x-1]-2*ut[x]+ut[x+1])*pow(N_in,2) + f(x,t))
+
+            ut= ut_next
+            t=t+delta_t
+            ut[0] = g1_in(t)
+            ut[N_in] = g2_in(t)
+            print(ut)
+            
---
+++ src/main.py
@@ -0,0 +1,25 @@
+import math
+from ex1 import Ex1
+
+def main():
+
+    #Ex1
+    f = lambda t,x : 10*x*x*(x-1) - 60*x*t + 20*t
+    u0 = lambda x : 0
+    g1 = lambda t : 0
+    g2 = lambda t : 0
+
+    print("\nN:")
+    N_in = int(input())
+
+    print("\nM:")
+    M_in = int(input())
+
+    print("\nlambda:")
+    lambda_in = float(input())
+
+    ex1 = Ex1()
+    ex1.resolver(u0,f,N_in,M_in,lambda_in,g1,g2)
+
+if __name__ == "__main__":
+    main()

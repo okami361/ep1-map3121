c7ffd78be0b9ab5140f7826bc0c39682b59e3b96
Vinicius Yu Okubo <vinnco@hotmail.com>
2020-05-01 19:46:56 -0300

    correção ex1
    acho que está certo

--- src/ex1.py
+++ src/ex1.py
@@ -7,22 +7,22 @@ class Ex1:
 
         ut = []
         t=0
-        ut.append(g1_in(t))
 
+        ut.append(g1_in(0))
         for xi in range(1,N_in):
-            ut.append(u0(xi))
+            ut.append(u0(xi*delta_x))
+        ut.append(g2_in(0))
 
-        ut.append(g2_in(t))
 
         for y in range(0, int(M_in)):
             ut_next = ut
             for xi in range(1,N_in):
-                ut_next[xi] =ut[xi] + delta_t*((ut[xi-1]-2*ut[xi]+ut[xi+1])*pow(N_in,2) + f(xi*delta_x,t))
+                ut_next[xi] =ut[xi] + delta_t*((ut[xi-1]-2*ut[xi]+ut[xi+1])/pow(delta_x,2) + f(t,xi*delta_x))
 
             ut= ut_next
             t=t+delta_t
             ut[0] = g1_in(t)
             ut[N_in] = g2_in(t)
-
+        
         return ut
             

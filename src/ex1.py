869e41119a45434a89e287bc11ef2803c4913804
Vinicius Yu Okubo <vinnco@hotmail.com>
2020-05-01 18:55:21 -0300

    correções do algoritmo

--- src/ex1.py
+++ src/ex1.py
@@ -3,25 +3,26 @@ class Ex1:
         
         
         delta_t = lambda_in/pow(N_in,2)
+        delta_x = 1/N_in
 
         ut = []
         t=0
         ut.append(g1_in(t))
 
-        for x in range(1,N_in):
-            ut.append(u0(x))
+        for xi in range(1,N_in):
+            ut.append(u0(xi))
 
         ut.append(g2_in(t))
 
         for y in range(0, int(M_in)):
             ut_next = ut
-            for x in range(1,N_in):
-                ut_next[x] = delta_t*((ut[x-1]-2*ut[x]+ut[x+1])*pow(N_in,2) + f(x,t))
+            for xi in range(1,N_in):
+                ut_next[xi] =ut[xi] + delta_t*((ut[xi-1]-2*ut[xi]+ut[xi+1])*pow(N_in,2) + f(xi*delta_x,t))
 
             ut= ut_next
             t=t+delta_t
             ut[0] = g1_in(t)
             ut[N_in] = g2_in(t)
 
-        print(ut)
+        return ut
             
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 from ex1 import Ex1
 
 def main():
@@ -9,6 +11,8 @@ def main():
     g1 = lambda t : 0
     g2 = lambda t : 0
 
+    f_exato = lambda t,x : 10*t*x*x*(x-1)
+
     print("\nN:")
     N_in = int(input())
 
@@ -18,7 +22,16 @@ def main():
     M = pow(N_in,2)/lambda_in
 
     ex1 = Ex1()
-    ex1.resolver(u0,f,N_in,M,lambda_in,g1,g2)
-
+    y_estimado = ex1.resolver(u0,f,N_in,M,lambda_in,g1,g2)
+    x = np.linspace(0,1,N_in+1)
+    plt.scatter(x,y_estimado)
+    
+    y_exato = []
+    for xi in x:
+        y_exato.append(f_exato(1, xi))
+      
+    plt.scatter(x,y_exato)
+    
+    plt.show()
 if __name__ == "__main__":
     main()

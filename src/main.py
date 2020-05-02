9b18cd5f36b9b8613a520c901d711834cc548d5c
Vinicius Yu Okubo <vinnco@hotmail.com>
2020-05-02 16:55:43 -0300

    ex1, função entregue, plot
    Adicionando a função a ser entregada e melorando como ela é plotada

--- src/main.py
+++ src/main.py
@@ -6,12 +6,19 @@ from ex1 import Ex1
 def main():
 
     #Ex1
-    f = lambda t,x : 10*x*x*(x-1) - 60*x*t + 20*t
-    u0 = lambda x : 0
+    #a
+    # f = lambda t,x : 10*x*x*(x-1) - 60*x*t + 20*t
+    # u0 = lambda x : 0
+    # g1 = lambda t : 0
+    # g2 = lambda t : 0
+
+    #a'
+    f = lambda t,x : 10*math.cos(10*t)*pow(x,2)*pow(1-x,2) - (1+math.sin(10*t))*(12*pow(x,2) - 12*x +2)
+    u0 = lambda x : pow(x,2)*pow(1-x,2)
     g1 = lambda t : 0
     g2 = lambda t : 0
 
-    f_exato = lambda t,x : 10*t*x*x*(x-1)
+    f_exato = lambda t,x : (1+math.sin(10*t))*pow(x,2)*pow(1-x,2)
 
     print("\nN:")
     N_in = int(input())
@@ -24,14 +31,30 @@ def main():
     ex1 = Ex1()
     y_estimado = ex1.resolver(u0,f,N_in,M,lambda_in,g1,g2)
     x = np.linspace(0,1,N_in+1)
-    plt.scatter(x,y_estimado)
-    
+    plt.plot(x,y_estimado, color='#0000FF') # cor azul
+
     y_exato = []
     for xi in x:
         y_exato.append(f_exato(1, xi))
-      
-    plt.scatter(x,y_exato)
-    
+
+    plt.plot(x,y_exato,color='#808080')  # cor cinza
+
+    plt.title('Distribuição do Calor em t=1')
+    plt.xlabel('x: Comprimento da Barra')
+    plt.ylabel('u(1,x)')
+
+    erro = 0
+    for i in range(len(y_estimado)):
+        e = abs (y_estimado[i] - y_exato[i])
+        if e > erro: 
+            erro = e
+    print("\nErro Máximo: ", erro)
+
     plt.show()
+
 if __name__ == "__main__":
-    main()
+    continuar = "s"
+    while continuar == "s": 
+        main()
+        print("\nContinuar? (s/n) ")
+        continuar = input()

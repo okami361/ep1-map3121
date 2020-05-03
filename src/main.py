092936ffa360973efd29a854d686b30c9c0f4a6d
Vinicius Yu Okubo <vinnco@hotmail.com>
2020-05-03 18:23:37 -0300

    testando o primeiro problema

--- src/main.py
+++ src/main.py
@@ -7,18 +7,20 @@ def main():
 
     #Ex1
     #a
-    # f = lambda t,x : 10*x*x*(x-1) - 60*x*t + 20*t
-    # u0 = lambda x : 0
-    # g1 = lambda t : 0
-    # g2 = lambda t : 0
-
-    #a'
-    f = lambda t,x : 10*math.cos(10*t)*pow(x,2)*pow(1-x,2) - (1+math.sin(10*t))*(12*pow(x,2) - 12*x +2)
-    u0 = lambda x : pow(x,2)*pow(1-x,2)
+    f = lambda t,x : 10*x*x*(x-1) - 60*x*t + 20*t
+    u0 = lambda x : 0
     g1 = lambda t : 0
     g2 = lambda t : 0
 
-    f_exato = lambda t,x : (1+math.sin(10*t))*pow(x,2)*pow(1-x,2)
+    f_exato = lambda t,x : 10*t*pow(x,2)*(x-1)
+
+    #a'
+    # f = lambda t,x : 10*math.cos(10*t)*pow(x,2)*pow(1-x,2) - (1+math.sin(10*t))*(12*pow(x,2) - 12*x +2)
+    # u0 = lambda x : pow(x,2)*pow(1-x,2)
+    # g1 = lambda t : 0
+    # g2 = lambda t : 0
+
+    #f_exato = lambda t,x : (1+math.sin(10*t))*pow(x,2)*pow(1-x,2)
 
     print("\nN:")
     N_in = int(input())
@@ -31,13 +33,13 @@ def main():
     ex1 = Ex1()
     y_estimado = ex1.resolver(u0,f,N_in,M,lambda_in,g1,g2)
     x = np.linspace(0,1,N_in+1)
-    plt.plot(x,y_estimado, color='#0000FF') # cor azul
+    plt.plot(x,y_estimado, color='#0000FF', label = 'estimado') # cor azul
 
     y_exato = []
     for xi in x:
         y_exato.append(f_exato(1, xi))
 
-    plt.plot(x,y_exato,color='#808080')  # cor cinza
+    plt.plot(x,y_exato,color='#808080', label = 'exato')  # cor cinza
 
     plt.title('Distribuição do Calor em t=1')
     plt.xlabel('x: Comprimento da Barra')
@@ -50,6 +52,7 @@ def main():
             erro = e
     print("\nErro Máximo: ", erro)
 
+    plt.legend(loc='best')
     plt.show()
 
 if __name__ == "__main__":

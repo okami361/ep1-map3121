447c87663ca2cf2e6aec7cd25a9a3bb3e7939bd9
Vinicius Yu Okubo <vinnco@hotmail.com>
2020-05-05 18:48:21 -0300

    progresso com ex2, metodo de Euler

--- src/ex2.py
+++ src/ex2.py
@@ -4,26 +4,96 @@ import numpy as np
 
 class Ex2:
 
-    def resolver(self,lambda_in,A_in):
-        diagonal_a = []
-        sub_diagonal_a = []
+
+    #TODO: segundo o roteiro é para ser usado N-1, mas não penso que faça sentido. Conferir quando possível
+    def resolver_euler(self, u0_in, f_in, g1_in, g2_in, N_in):
+        
+        delta_t = delta_x = 1/N_in
+        lambda_calc = 1/delta_x
+        
+        a_diagonal, a_sub_diagonal = self.calc_decomp(self.gerar_matriz(N_in, lambda_calc))
+
+        ut = []
+        ut.append(g1_in(0))
+        for xi in range(1,N_in-1):
+            ut.append(u0_in(xi*delta_x))
+        ut.append(g2_in(0))
+
+
+        for t in range(0, 1, delta_t):
+            ut[0] = ut[0] + lambda_calc*g1_in(t+delta_t)
+            ut[N_in-1] = ut[N_in-1] + lambda_calc*g2_in(t+delta_t)
+
+            for x in range(0,N_in):
+                ut[x] = ut[x] + delta_t*f_in(t+delta_t,x*delta_x)
+            
+            temp = self.substituicao_direta(a_sub_diagonal, ut)
+            temp = self.resolver_diagonal(a_diagonal, temp)
+
+
+        return None
+
+
+    #https://themadcreator.github.io/luqr/
+    #Ax=B, usado para a função L
+    def substituicao_direta(self, sub_diagonal, b):
+        
+        if(len(sub_diagonal-1) != len(b)):
+            raise ValueError('Dimensoes impossiveis de serem resolvidas') 
+        
+        resolvido = [b[0]]
+
+        for i in range(1,len(b)):
+            resolvido.append(b[i]-sub_diagonal[i-1])
+
+
+        return resolvido
+
+    def resolver_diagonal(self, diagonal, y):
+
+        if(len(diagonal) != len(y)):
+            raise ValueError('Dimensoes impossiveis de serem resolvidas') 
+
+        resolvido = []
+        for i in range(len(diagonal)):
+            resolvido.append(y[i]/diagonal[i])
+
+        return resolvido
+
+
+
+    def gerar_matriz(self, N_in, lambda_in):
+        A = np.identity(N_in -1)
+        for i in range(0,len(A)):
+            A[i][i] = 1+2*lambda_in
+
+        for i in range(0,len(A)-1):
+            A[i+1][i] = -lambda_in
+            A[i][i+1] = -lambda_in
+        
+        return A
+
+    def calc_decomp(self,A_in):
+        diagonal = []
+        sub_diagonal = []
         
         for i in range(0,len(A_in)-1):
             aux_matriz = np.identity(len(A_in))
             aux_indice = A_in[i+1][i]/A_in[i][i]
-            sub_diagonal_a.append(aux_indice)
+            sub_diagonal.append(aux_indice)
             aux_matriz[i+1][i] = -aux_indice
             
             A_in = aux_matriz.dot(A_in)
 
         for i in range(0,len(A_in)):
-            diagonal_a.append(A_in[i][i])
+            diagonal.append(A_in[i][i])
 
-        self.calc_LDL(diagonal_a, sub_diagonal_a)
+        self.conferir_LDL(diagonal, sub_diagonal)
 
-        return None
+        return diagonal, sub_diagonal
 
-    def calc_LDL(self, diagonal, sub_diagonal):
+    #Fiz mais para conferir o cálculo
+    def conferir_LDL(self, diagonal, sub_diagonal):
         L = np.identity(len(diagonal))
         D = np.identity(len(diagonal))
 
@@ -34,9 +104,5 @@ class Ex2:
             L[i+1][i] = sub_diagonal[i]
 
         print(L.dot(D).dot(L.transpose()))
-
-        return None
-
-    def resolver_euler(self,lambda_in,A_in):
 
         return None
--- src/main.py
+++ src/main.py
@@ -9,79 +9,79 @@ def main():
 
     #Ex1
     #a
-    # f = lambda t,x : 10*x*x*(x-1) - 60*x*t + 20*t
-    # u0 = lambda x : 0
-    # g1 = lambda t : 0
-    # g2 = lambda t : 0
+    f = lambda t,x : 10*x*x*(x-1) - 60*x*t + 20*t
+    u0 = lambda x : 0
+    g1 = lambda t : 0
+    g2 = lambda t : 0
 
-    # f_exato = lambda t,x : 10*t*pow(x,2)*(x-1)
+    f_exato = lambda t,x : 10*t*pow(x,2)*(x-1)
 
-    # #a'
-    # f = lambda t,x : 10*math.cos(10*t)*pow(x,2)*pow(1-x,2) - (1+math.sin(10*t))*(12*pow(x,2) - 12*x +2)
-    # u0 = lambda x : pow(x,2)*pow(1-x,2)
-    # g1 = lambda t : 0
-    # g2 = lambda t : 0
-
-
-    # f_exato = lambda t,x : (1+math.sin(10*t))*pow(x,2)*pow(1-x,2)
-
-    # print("\nN:")
-    # N_in = int(input())
-
-    # print("\nlambda:")
-    # lambda_in = float(input())
-
-    # M = math.ceil(pow(N_in,2)/lambda_in)
-    # lambda_calc = math.pow(N_in,2)/M
+    #a'
+    f = lambda t,x : 10*math.cos(10*t)*pow(x,2)*pow(1-x,2) - (1+math.sin(10*t))*(12*pow(x,2) - 12*x +2)
+    u0 = lambda x : pow(x,2)*pow(1-x,2)
+    g1 = lambda t : 0
+    g2 = lambda t : 0
 
 
+    f_exato = lambda t,x : (1+math.sin(10*t))*pow(x,2)*pow(1-x,2)
 
-    # ex1 = Ex1()
-    # x = np.linspace(0,1,N_in+1)
-    # y_estimado = ex1.resolver(u0,f,N_in,M,lambda_calc,g1,g2,x,plt)
-    # plt.plot(x,y_estimado, color='#0000FF', label = 'estimado') # cor azul
-
-    # y_exato = []
-    # for xi in x:
-    #     y_exato.append(f_exato(.5, xi))
-
-    # plt.plot(x,y_exato,color='#808080', label = 'exato')  # cor cinza
-
-    # plt.title('Distribuição do Calor em t=1')
-    # plt.xlabel('x: Comprimento da Barra')
-    # plt.ylabel('u(1,x)')
-
-    # erro = 0
-    # for i in range(len(y_estimado)):
-    #     e = abs (y_estimado[i] - y_exato[i])
-    #     if e > erro: 
-    #         erro = e
-    # print("\nErro Máximo: ", erro)
-
-    # plt.legend(loc='best')
-    # plt.show()
-
-
-
-    #Ex2
-    ex2 = Ex2()
     print("\nN:")
     N_in = int(input())
 
     print("\nlambda:")
     lambda_in = float(input())
 
-    A = np.identity(N_in -1)
-    for i in range(0,len(A)):
-        A[i][i] = 1+2*lambda_in
+    M = math.ceil(pow(N_in,2)/lambda_in)
+    lambda_calc = math.pow(N_in,2)/M
 
-    for i in range(0,len(A)-1):
-        A[i+1][i] = -lambda_in
-        A[i][i+1] = -lambda_in
 
-    print(A)
 
-    ex2.resolver(0,A)
+    ex1 = Ex1()
+    x = np.linspace(0,1,N_in+1)
+    y_estimado = ex1.resolver(u0,f,N_in,M,lambda_calc,g1,g2,x,plt)
+    plt.plot(x,y_estimado, color='#0000FF', label = 'estimado') # cor azul
+
+    y_exato = []
+    for xi in x:
+        y_exato.append(f_exato(.5, xi))
+
+    plt.plot(x,y_exato,color='#808080', label = 'exato')  # cor cinza
+
+    plt.title('Distribuição do Calor em t=1')
+    plt.xlabel('x: Comprimento da Barra')
+    plt.ylabel('u(1,x)')
+
+    erro = 0
+    for i in range(len(y_estimado)):
+        e = abs (y_estimado[i] - y_exato[i])
+        if e > erro: 
+            erro = e
+    print("\nErro Máximo: ", erro)
+
+    plt.legend(loc='best')
+    plt.show()
+
+
+
+    #Ex2
+    # ex2 = Ex2()
+    # print("\nN:")
+    # N_in = int(input())
+
+    # print("\nlambda:")
+    # lambda_in = float(input())
+
+    # A = np.identity(N_in -1)
+    # for i in range(0,len(A)):
+    #     A[i][i] = 1+2*lambda_in
+
+    # for i in range(0,len(A)-1):
+    #     A[i+1][i] = -lambda_in
+    #     A[i][i+1] = -lambda_in
+
+    # print(A)
+
+    # ex2.resolver(0,A)
 
 if __name__ == "__main__":
     continuar = "s"
